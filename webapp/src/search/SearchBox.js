62c393baade1c2fc903ec67a4333bf7ccb0c474b
hanzbzz <honza.brazda2@seznam.cz>
2025-04-19 16:01:15 +0200

    Remove line to demo CI/CD

--- webapp/src/search/SearchBox.js
+++ webapp/src/search/SearchBox.js
@@ -3,7 +3,7 @@ import React from "react";
 export function SearchBox(props) {
     return <div className="row">
         <div className="input-field col s12">
-            <input placeholder="Type skill name to filter... PA200 HW2 TEST"
+            <input placeholder="Type skill name to filter..."
                    id="search_string" type="text" className="validate"
                    onChange={e => props.updateSearch(e.target.value)}
                    value={props.search}
