7fc20ae43d3f7db7980342fcd8858942a6a23e24
hanzbzz <honza.brazda2@seznam.cz>
2025-04-19 15:03:55 +0200

    Remove subject from search bar

--- webapp/src/search/SearchBox.js
+++ webapp/src/search/SearchBox.js
@@ -3,7 +3,7 @@ import React from "react";
 export function SearchBox(props) {
     return <div className="row">
         <div className="input-field col s12">
-            <input placeholder="Type skill name to filter... PA2002"
+            <input placeholder="Type skill name to filter..."
                    id="search_string" type="text" className="validate"
                    onChange={e => props.updateSearch(e.target.value)}
                    value={props.search}
